d8d143ee006f9e293397d8a826106cc52076a53c
Topias Heinonen <corvus@iki.fi>
2014-05-09 17:25:28 +0300

    Kalenterin luonti melkein toimii, mutta ei kuitenkaan.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,12 +2,22 @@
 
 var app = angular.module('massiveWallhackApp');
 
-app.controller('MainCtrl', function ($scope, Courses) {
+app.controller('MainCtrl', function ($scope, Courses, Uuid, Calendar) {
   $scope.events = {};
   $scope.eventKeys = [];
+  $scope.uuid = Uuid.generate();
+  $scope.calLink = Calendar.getLink($scope.uuid);
+  $scope.showEvents = false;
   Courses.all().success(function (data, status, headers, config) {
     console.log(data);
     $scope.courses = data;
+  });
+
+  Calendar.get("sa").success(function(data,status, headers, config){
+    console.log(data);
+    console.log(status);
+    console.log(headers);
+    console.log(config);
   });
 
   $scope.addOrRemoveFromEvents = function (checked, course) {
@@ -25,6 +35,14 @@ app.controller('MainCtrl', function ($scope, Courses) {
     }
 
     $scope.eventKeys = Object.keys($scope.events);
+
+    Calendar.create($scope.uuid, $scope.events).success(function(data,status, headers, config){
+      console.log(data);
+      console.log(status);
+      console.log(headers);
+      console.log(config);
+      $scope.showEvents = true;
+    });
 
     Object.keys($scope.events).forEach(function (eventId) {
       console.log($scope.events[eventId]);
--- app/scripts/services.js
+++ app/scripts/services.js
@@ -1,12 +1,55 @@
 var app = angular.module('massiveWallhackApp');
 
 app.factory('Courses', function($http) {
-    var URL = 'http://localhost:3000/courses.json';
+  var URL = 'http://localhost:3000/courses.json';
 
-    var coursesService = {};
-    coursesService.all = function() {
-        return $http.get(URL);
-      }
+  var coursesService = {};
+  coursesService.all = function() {
+    return $http.get(URL);
+  };
 
-    return coursesService;
-  });
+  return coursesService;
+});
+
+app.factory('Calendar', function($http) {
+  var baseURL = 'http://localhost:3001/';
+  var calendarService = {};
+
+  function link(uuid) {
+    return baseURL + uuid;
+  }
+
+  calendarService.create = function(uuid, events) {
+    var obj = {};
+    obj.uuid = uuid;
+    obj.events = events;
+
+    return $http.post(baseURL + "event_lists.json", obj);
+  };
+
+  calendarService.get = function(uuid) {
+    return $http.get(link(uuid) + ".ics");
+  }
+
+  calendarService.getLink = function(uuid) {
+    return link(uuid);
+  }
+
+  return calendarService;
+});
+
+app.factory('Uuid', function() {
+  var fmt = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx';
+
+  var uuidService = {};
+
+  uuidService.generate = function() {
+    /**! http://stackoverflow.com/a/2117523/377392 */
+    return fmt.replace(/[xy]/g, function(c) {
+      var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
+    });
+  };
+
+  return uuidService;
+});
