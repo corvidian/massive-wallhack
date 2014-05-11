18d740aefca9e67c5cfc264d4ba26f62869605ca
Travis <travis@travis-ci.org>
2014-05-12 01:52:00 +0300

    Kaunis on.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,13 +8,28 @@ app.controller('MainCtrl', function ($scope, Courses, Uuid, Calendar) {
   $scope.uuid = Uuid.generate();
   $scope.calLink = Calendar.getLink($scope.uuid);
   $scope.showEvents = false;
-  Courses.all().success(function (data, status, headers, config) {
+  $scope.saved = false;
+
+  Courses.all().success(function (data) {
     console.log(data);
     $scope.courses = data.courses;
+    $scope.courses.forEach(function(course) {
+      course.exercise_groups.forEach(function(group){
+        group.checked = false;
+      });
+    });
   });
 
-  $scope.addOrRemoveFromEvents = function (checked, course, group) {
-    if (checked) {
+  $scope.click = function($event) {
+    $event.stopPropagation();
+  };
+
+  $scope.addOrRemoveFromEvents = function (course, group, $event) {
+    $scope.saved = false;
+    $scope.saveError = false;
+    $scope.saving = true;
+    console.log("Checked: " + group.checked);
+    if (group.checked) {
       group.exercises.forEach(function (lecture) {
         lecture.name = course.name + ' ' + group.name;
         $scope.events[lecture.event_id] = lecture;
@@ -29,13 +44,16 @@ app.controller('MainCtrl', function ($scope, Courses, Uuid, Calendar) {
 
     $scope.eventKeys = Object.keys($scope.events);
 
+
     Calendar.create($scope.uuid, $scope.events).success(function (data, status, headers, config) {
       console.log(data);
       console.log(status);
       console.log(headers);
       console.log(config);
       $scope.showEvents = true;
+      $scope.saving = false;
+      if (status == 201) $scope.saved = true;
+      else $scope.saveError = true;
     });
-
   };
 });
--- tuntikirjanpito.md
+++ tuntikirjanpito.md
@@ -6,4 +6,6 @@ Pvm    | Aika | Edistyminen
 5.5.   |10h   |Tutoriaaleja yms.
 6.5.   |8h    |Oman projektin init, backend antamaan vähän dataa
 8.5.   |8h    |Kurssien ja luentojen listaus ja valinta, aloitettu kalenterin rakentaminen
-9.5.   |12h    |Kalenterien rakentaminen
+9.5.   |12h   |Kalenterien rakentaminen
+10.5.  |4h    |Laskarit backendiin
+11.5.  |7h    |Laskarit fronttiin, frontin kaunistamista
